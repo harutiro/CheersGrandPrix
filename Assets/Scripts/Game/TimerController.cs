3ed74bc44f44e6bba1dc3ec215e25c95df404658
harutiro <harutiro2027@outlook.jp>
2023-09-24 21:20:13 +0900

    add: 終了する時にSEが流れるように変更

--- Assets/Scripts/Game/TimerController.cs
+++ Assets/Scripts/Game/TimerController.cs
@@ -19,18 +19,66 @@ public class TimerController : MonoBehaviour
     /// タイマーの現在値
     /// </summary>
     private float totalTime;
+    
+    /// <summary>
+    /// 終了後に表示するテキストの最大値
+    /// </summary>
+    public float MaxfinishTime = 5.0f;
+
+    /// <summary>
+    /// 終了のタイマー時間
+    /// </summary>
+    private float finishTime;
+    
+    /// <summary>
+    /// 終了用のテキストを扱うやつ
+    /// </summary>
+    public Text FinishText;
+    
+    /// <summary>
+    /// cupのオブジェクト
+    /// </summary>
+    public GameObject cup;
         
+    /// <summary>
+    ///  cupのAudioSource
+    /// </summary>
+    private AudioSource cupAudioSource;
+    
+    /// <summary>
+    /// 終了ようSE
+    /// </summary>
+    public AudioClip finishAudioClip;
     
     // Start is called before the first frame update
     void Start()
     {
         totalTime = maxTime;
+        finishTime = MaxfinishTime;
+        FinishText.enabled = false;
+        
+        cupAudioSource = cup.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (totalTime <= 0.0f)
+        {
+            // SEを鳴らす
+            if (finishTime == MaxfinishTime)
+            {
+                cupAudioSource.PlayOneShot(finishAudioClip);
+            }
+            
+            // フィニッシュテキストを有効かさせる
+            FinishText.enabled = true;
+            GameManager.isGameStart = false;
+            
+            // 終了用のタイマーを減らす
+            finishTime -= Time.deltaTime;
+        }
+        if (totalTime <= 0.0f && finishTime <= 0.0f)
         {
             // タイマーが0になったらゲーム終了
             SceneController.LoadScene(SceneController.SceneName.Result);
